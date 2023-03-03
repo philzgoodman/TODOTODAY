6bad80f63094dd4e8eb70f6bdb3d0fe7dbe853fc
philzgoodman <philzgoodmantv@gmail.com>
2023-03-03 11:43:53 -0800

    f

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "533a65d72ed434c297c44eacba1fe5d4",
 "icons/Icon-maskable-192.png": "a492fe3e06e60101aced07d448a2efbc",
 "icons/Icon-maskable-512.png": "20725c7d677c79f7e67e425230c24ad2",
-"index.html": "fc1ea389e3f64fc5aa64425b5b641491",
-"/": "fc1ea389e3f64fc5aa64425b5b641491",
+"index.html": "d97b5b3fe6240fcbc05743673b2f899a",
+"/": "d97b5b3fe6240fcbc05743673b2f899a",
 "main.dart.js": "16eee61740263f8380e818c0667775a2",
 "manifest.json": "689a9eaa9b64954a8048d19561372e86",
 "version.json": "30e616f587a4aabc4ab0a298f5de93a8"
